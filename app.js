7f4f79a1bed8696333556d29019aec6cfc1818ee
Arshad Mustafa <70991332+arshadm2001@users.noreply.github.com>
2024-07-03 18:46:47 +0000

    Message

---
+++ app.js
@@ -0,0 +1,17 @@
+console.log("CarApp initialized");
+
+// Basic structure for your app
+class CarApp {
+    constructor() {
+        this.init();
+    }
+
+    init() {
+        console.log("Initializing CarApp...");
+        // Add initialization logic here
+    }
+
+    // Add more methods as needed
+}
+
+const app = new CarApp();
